a04c1da04733bf6a363bf2cf94132f6691789053
dmitry <ya.yarochkin@yandex.ru>
2022-07-24 21:14:28 +0300

    regex todo deal with ' style

--- scripts/kivy/main.py
+++ scripts/kivy/main.py
@@ -10,6 +10,7 @@ from nltk.stem import PorterStemmer
 import eng_to_ipa as p
 from googletrans import Translator
 import datetime
+import re
 
 time = f"{datetime.datetime.now()}.csv"
 lemmatizer = WordNetLemmatizer()
@@ -25,7 +26,8 @@ class TranslatorApp(App):
                             padding=10
                             )
         self.INPUT = TextInput(
-            text='put your text here: example\nIn summary, our results reveal robust neuronal and high-gamma auditory responses during sleep in Heschl’s gyrus and also in the anterior Superior Temporal Gyrus (STG), planum polare and middle temporal gyrus—well beyond early auditory cortex.')
+            text='In summary, our results reveal robust neuronal and high-gamma auditory responses during sleep in Heschl’s gyrus and also in the anterior Superior Temporal Gyrus (STG), planum polare and middle temporal gyrus—well beyond early auditory cortex.')
+
         self.OUTPUT = TextInput(text=f"list to learn,{time} \n " + chr(2200))
         submit = Button(text='Translate', on_press=self.submit)
         printCSV = Button(text='Скачать список CSV', on_press=self.printCSV)
@@ -37,6 +39,7 @@ class TranslatorApp(App):
 
     def submit(self, obj):
         file = self.INPUT.text
+
         ps = PorterStemmer()
         stopwords = set(line.strip() for line in open('../../txt/stopwords.txt'))
         # stopwords = stopwords.union(set(['mr','mrs','one','two','said']))
@@ -45,23 +48,17 @@ class TranslatorApp(App):
         wordcount = {}
         # To eliminate duplicates, remember to split by punctuation, and use case demiliters.
         for word in file.lower().split():
-            word = word.replace(".", "")
-            word = word.replace(",", "")
-            word = word.replace(":", "")
-            word = word.replace("\"", "")
-            word = word.replace("!", "")
-            word = word.replace("*", "")
-            word = word.replace("`", "")
-            word = word.replace("'", " ")
-            word = word.replace("?", " ")
+            regex = re.compile('[^a-zA-Z\'\s-]')
+            word = regex.sub('', word)
 
-            if word not in stopwords:
-                if word not in wordcount:
-                    wordcount[word] = 1
-                else:
-                    wordcount[word] += 1
+
+        if word not in stopwords:
+            if word not in wordcount:
+                wordcount[word] = 1
+            else:
+                wordcount[word] += 1
         # Print most common word
-        n_print = 4
+        n_print = 10
         word_counter = collections.Counter(wordcount)
         for word, count in word_counter.most_common(n_print):
             translator = Translator()
